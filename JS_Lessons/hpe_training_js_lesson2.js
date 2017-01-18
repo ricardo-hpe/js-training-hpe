33d4268865d4deb18be52ffb3902c01a6f8e4d1d
ricardo-hpe <ricardo.gaizka-arostegui-bran@hpe.com>
2017-01-18 11:50:09 -0600

    JS - Lesson 2
    This commit contains the second practice for hpe - js - training.

---
+++ JS_Lessons/hpe_training_js_lesson2.js
@@ -0,0 +1,14 @@
+/* Hi all and welcome to hpe training.
+This is the lesson number 2*/
+
+/*Create a .js file and name it like this: hpe_training_js_lesson2_<your name>.js
+Go to https://github.com/ricardo-hpe/js-training-hpe and review the content inside the README.md file.*/
+
+/* Exercises for lesson 2:
+1.- Using Logical Operators, write 6 comparations, 3 that returns true and 3 others that return false. It can be either a comparation between a length of a String and a number, or just between numbers.
+2.- Using Substrings and this coordinates: (0,2), (2,3), (0,5), (1,3), (5,7), Write 5 different sentences and print out the output.
+3.- Declare 5 Arrays and print only one of the values inside, it can be any of them.
+4.- Write 6 simple if's, where if the condition is true, prints this: "I'm doing it in the right way"
+5.- Based on Modulo operations, write 6 sentences where 3 of them returns 0 as result and 3 others with 1 as the output. Try to use big numbers ;-)*/
+
+// TIP: If you wanna review your results, go to http://www.webtoolkitonline.com/javascript-tester.html
