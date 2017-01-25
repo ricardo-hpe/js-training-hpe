bbd31154e0924550244f9250b202e6c817f61459
ricardo-hpe <ricardo.gaizka-arostegui-bran@hpe.com>
2017-01-25 14:38:26 -0600

    js-hpe-training
    This commit contains the lesson number 3 for JavaScript training.

---
+++ JS_Lessons/hpe_training_js_lesson3.js
@@ -0,0 +1,18 @@
+/* Hi all and welcome to hpe training.
+This is the lesson number 3*/
+
+/*Create a .js file and name it like this: hpe_training_js_lesson3_<your name>.js
+Go to https://github.com/ricardo-hpe/js-training-hpe and review the content inside the README.md file.*/
+
+/*
+Exercises for lesson 3:
+1.- Using a For loop, print out numbers from 0 to 100 with a increase of 5 each.
+2.- Using a For loop, print all the values of an array by separate. Tip: You will need to use .length ;-)
+3.- Using a For loop, print out numbers from 24 to 2 with a decrease of 2 each.
+4.- Using an If statement, generate a condition that includes a .length comparation, and print numbers from 10 to 50 using for.
+    Include an else that prints the same values but decreasing.
+5.-Using an If statement, generate a similar condition as in excercise 2, and print numbers from 9 to 0 using for.
+    Include an else that prints the same values but increasing.
+*/
+
+// HotTip: Wanna actually see console.log results? Then go to https://jsbin.com/metozelize/edit?js,console,output
