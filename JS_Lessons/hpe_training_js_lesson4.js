0c56011029dc8794b69929f748f61a403eefaa08
ricardo-hpe <ricardo.gaizka-arostegui-bran@hpe.com>
2017-02-02 16:17:05 -0600

    Lesson 4

--- JS_Lessons/hpe_training_js_lesson4.js
+++ JS_Lessons/hpe_training_js_lesson4.js
@@ -6,7 +6,14 @@ Go to https://github.com/ricardo-hpe/js-training-hpe and review the content insi
 
 /*
 Exercises for lesson 4:
-1. Create a simple function.
+1. Create a simple function that prints out this line: "My name is: <yourName> and guess what?
+I'm indeed learning js" and call it sending your name as a parameter.
+2. Create a simple While loop that prints true until the condition becomes false.
+3. Create a While loop that prints all the statements inside an array of at least 10 words/sentences.
+4. Combine a While and For, the For loop is gonna be isnide the While loop. Tip: take care of the infinite loops ;)
+5. Ask a user for a number between 1-100 and create an If, elsif, else statement where you can print something different
+depending on his/her choice. make sure you are making all this inside a function and the parameter that is gonna be sent, is the
+user's answer. Tip: prompt is gonna help you out with this.
 */
 
 // HotTip: Wanna actually see console.log results? Then go to https://jsbin.com/metozelize/edit?js,console,output
