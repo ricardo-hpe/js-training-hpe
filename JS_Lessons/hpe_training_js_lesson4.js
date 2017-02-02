775977ad14db370d5f80601b083364e5b6b14771
ricardo-hpe <ricardo.gaizka-arostegui-bran@hpe.com>
2017-02-02 15:40:38 -0600

    Test commit 1
    blah blah blah

---
+++ JS_Lessons/hpe_training_js_lesson4.js
@@ -0,0 +1,11 @@
+/* Hi all and welcome to hpe training.
+This is the lesson number 4*/
+
+/*Create a .js file and name it like this: hpe_training_js_lesson3_<your name>.js
+Go to https://github.com/ricardo-hpe/js-training-hpe and review the content inside the README.md file.*/
+
+/*
+Exercises for lesson 4:
+*/
+
+// HotTip: Wanna actually see console.log results? Then go to https://jsbin.com/metozelize/edit?js,console,output
